c5a308ff03e48a77704e10d03dbdf96a315ef05a
jonathan <yonathan.s@gmf-aeroasia.co.id>
2025-03-13 21:30:55 +0700

    [update]task 1, task 2 partial component, readme update, env local adjustment

--- backend/src/modules/students/students-controller.js
+++ backend/src/modules/students/students-controller.js
@@ -2,28 +2,35 @@ const asyncHandler = require("express-async-handler");
 const { getAllStudents, addNewStudent, getStudentDetail, setStudentStatus, updateStudent } = require("./students-service");
 
 const handleGetAllStudents = asyncHandler(async (req, res) => {
-    //write your code
-
+    const {name, className, section, roll} = req.query;
+    const students = await getAllStudents({name, className, section, roll});
+    res.json(students);
 });
 
 const handleAddStudent = asyncHandler(async (req, res) => {
-    //write your code
-
+    const payload = req.body;
+    const result = await addNewStudent(payload);
+    res.json(result);
 });
 
 const handleUpdateStudent = asyncHandler(async (req, res) => {
-    //write your code
-
+    const { id: userId } = req.params;
+    const payload = req.body;
+    const result = await updateStudent({ ...payload, userId });
+    res.json(result);
 });
 
 const handleGetStudentDetail = asyncHandler(async (req, res) => {
-    //write your code
-
+    const accountDetail = await getStudentDetail(req.params.id);
+    res.json(accountDetail);
 });
 
 const handleStudentStatus = asyncHandler(async (req, res) => {
-    //write your code
-
+    const status = req.body.status;
+    const { id: reviewerId } = req.user;
+    const { id: userId } = req.params;
+    const result = await setStudentStatus({status, userId, reviewerId});
+    res.json(result);
 });
 
 module.exports = {
